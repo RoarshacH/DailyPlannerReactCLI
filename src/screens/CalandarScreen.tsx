57ba986d7efc6e06974042db0842d6a40c41c1e8
RoarshacH <smv.dilshan@gmail.com>
2022-08-01 13:47:34 -0400

    refactor home and calandar

--- src/screens/CalandarScreen.tsx
+++ src/screens/CalandarScreen.tsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { View, StyleSheet } from "react-native";
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 
-import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 const CalendarScreen = () => {
   return (
@@ -11,15 +11,19 @@ const CalendarScreen = () => {
           style={styles.calandar}
           markingType="multi-period"
           markedDates={{
-            "2017-12-14": {
+            '2017-12-14': {
               periods: [
-                { startingDay: false, endingDay: true, color: "#5f9ea0" },
-                { startingDay: false, endingDay: true, color: "#ffa500" },
-                { startingDay: true, endingDay: false, color: "#f0e68c" },
+                {startingDay: false, endingDay: true, color: '#5f9ea0'},
+                {startingDay: false, endingDay: true, color: '#ffa500'},
+                {startingDay: true, endingDay: false, color: '#f0e68c'},
               ],
             },
-            "2017-12-15": {
-              periods: [{ startingDay: true, endingDay: false, color: "#ffa500" }, { color: "transparent" }, { startingDay: false, endingDay: false, color: "#f0e68c" }],
+            '2017-12-15': {
+              periods: [
+                {startingDay: true, endingDay: false, color: '#ffa500'},
+                {color: 'transparent'},
+                {startingDay: false, endingDay: false, color: '#f0e68c'},
+              ],
             },
           }}
         />
@@ -31,16 +35,16 @@ const CalendarScreen = () => {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-    flexDirection: "column",
+    flexDirection: 'column',
   },
   content: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   calandar: {
     flex: 1,
     width: 400,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
 });
 
